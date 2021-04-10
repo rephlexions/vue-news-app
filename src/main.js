160cd2bb114b281599c914c03d44714f6c79259e
Ovidiu Cara <ovidiu92@gmail.com>
2021-04-10 13:47:33 +0200

    Project set up

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
+/* eslint-disable */ 
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/saga-blue/theme.css"       //theme
+import "primevue/resources/primevue.min.css"                 //core css
+import "primeicons/primeicons.css"     
+import 'primeflex/primeflex.css';                      //icons
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(PrimeVue);
+app.mount("#app");
