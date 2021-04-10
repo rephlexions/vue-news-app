7343d88fb8211b477f78002c6fda2da7f3c312d0
Ovidiu Cara <ovidiu92@gmail.com>
2021-04-10 17:25:36 +0200

    main content component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Toolbar>
+        <template #left>
+            <Button icon="pi pi-bars" @click="visibleLeft = true" />
+            <div id="title" class="p-text-normal p-ml-4">News Reader</div>
+        </template>
+    </Toolbar>
+
+    <Sidebar v-model:visible="visibleLeft">Feeds</Sidebar>
+    <div class="p-grid">
+        <div class="p-md-4 p-md-offset-4 p-sm-12 p-sm-offset-1">
+            <MainContent :articles="articles"></MainContent>
+        </div>
+    </div>
+    <ScrollTop />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios"
+import { API_KEY } from "./env"
+import Sidebar from "primevue/sidebar"
+import Button from "primevue/button"
+import Toolbar from "primevue/toolbar"
+import MainContent from "./components/MainContent"
+import ScrollTop from "primevue/scrolltop"
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+    name: "App",
+    components: {
+        Sidebar,
+        Button,
+        Toolbar,
+        MainContent,
+        ScrollTop,
+    },
+    data() {
+        return {
+            visibleLeft: false,
+            articles: [],
+            errors: [],
+            API_KEY: API_KEY,
+        }
+    },
+    created() {
+        axios
+            .get(
+                "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=" +
+                    this.API_KEY
+            )
+            .then((response) => {
+                //this.articles = response.data.articles
+                this.articles = response.data.articles
+                console.log("data:")
+                console.log(response.data.articles) // This will give you access to the full object
+            })
+            .catch((e) => {
+                this.errors.push(e)
+            })
+    },
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div
+        v-for="article in articles"
+        :key="article.title"
+        class="article p-shadow-4"
+    >
+        <Card>
+            <template #header>
+                <img alt="user header" v-bind:src="article.urlToImage" />
+            </template>
+            <template #title>{{ article.title }}</template>
+            <template #content>
+                {{ article.description }}
+            </template>
+            <template #footer>
+                <Tag value="">{{ article.source.id }}</Tag>
+                <a :href="article.url" target="_blank">
+                    <Button icon="pi" label="Read more" />
+                </a>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script>
+import Card from "primevue/card"
+import Button from "primevue/button"
+import Tag from "primevue/tag"
+
+export default {
+    name: "MainContent",
+    props: {
+        articles: Array,
+    },
+    components: {
+        Card,
+        Button,
+        Tag,
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+    text-decoration: none;
+}
+.article {
+    margin-bottom: 2em;
+}
+</style>
